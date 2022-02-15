8e4647157520ac81b921c41d31deba4d51af1ebd
Jorgsaa <jorgen.saanum@gmail.com>
2022-02-15 10:23:53 +0100

    FEAT: implemented the Character base class

--- src/main/java/com/jorgsaa/character/Character.java
+++ src/main/java/com/jorgsaa/character/Character.java
@@ -1,5 +1,31 @@
 package com.jorgsaa.character;
 
+import com.jorgsaa.attribute.PrimaryAttribute;
+
 public abstract class Character {
 
+    private final String name;
+    private Integer level;
+
+    protected Character(String name) {
+        this.name = name;
+    }
+
+    public abstract PrimaryAttribute getBasePrimaryAttributes();
+
+    public abstract PrimaryAttribute getTotalPrimaryAttributes();
+
+    protected abstract Double getCharacterDamage();
+
+    public Double getDamage() {
+        return 0d;
+    }
+
+    public Integer getLevel() {
+        return level;
+    }
+
+    public void setLevel(Integer level) {
+        this.level = level;
+    }
 }
