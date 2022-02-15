eae1dfd7d7483e022aca190060537b93346d81f7
Jorgsaa <jorgen.saanum@gmail.com>
2022-02-15 11:30:03 +0100

    FEAT: implemented class Item

--- src/main/java/com/jorgsaa/item/Item.java
+++ src/main/java/com/jorgsaa/item/Item.java
@@ -1,4 +1,25 @@
 package com.jorgsaa.item;
 
 public abstract class Item {
+    private final String name;
+    private final Integer requiredLevel;
+    private final Slot slot;
+
+    protected Item(String name, Integer requiredLevel, Slot slot) {
+        this.name = name;
+        this.requiredLevel = requiredLevel;
+        this.slot = slot;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getRequiredLevel() {
+        return requiredLevel;
+    }
+
+    public Slot getSlot() {
+        return slot;
+    }
 }
