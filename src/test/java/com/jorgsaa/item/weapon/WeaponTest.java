b23d360eba1cae8bf6b44575d9f2b5fa0ed14be0
Jorgsaa <jorgen.saanum@gmail.com>
2022-02-15 16:13:38 +0100

    TEST: added test cases for class `Weapon`

--- src/test/java/com/jorgsaa/item/weapon/WeaponTest.java
+++ src/test/java/com/jorgsaa/item/weapon/WeaponTest.java
@@ -1,5 +1,48 @@
 package com.jorgsaa.item.weapon;
 
+import com.jorgsaa.item.Slot;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class WeaponTest {
 
+    private static final String ORIGINAL_NAME = "Steel axe";
+    private static final int ORIGINAL_LEVEL = 10;
+    private static final WeaponType ORIGINAL_TYPE = WeaponType.AXE;
+    private static final double ORIGINAL_DAMAGE = 10d;
+    private static final double ORIGINAL_ATTACK_SPEED = 1.3d;
+
+    private Weapon weapon;
+
+    @BeforeEach
+    public void setUp() {
+        weapon = new Weapon(ORIGINAL_NAME, ORIGINAL_LEVEL, ORIGINAL_TYPE, ORIGINAL_DAMAGE, ORIGINAL_ATTACK_SPEED);
+    }
+
+    @Test
+    public void getDPS_ReturnsCorrectValue() {
+        assertEquals(ORIGINAL_DAMAGE * ORIGINAL_ATTACK_SPEED, weapon.getDPS());
+    }
+
+    @Test
+    public void getType_ReturnsOriginalType() {
+        assertEquals(ORIGINAL_TYPE, weapon.getType());
+    }
+
+    @Test
+    public void getName_ReturnsOriginalName() {
+        assertEquals(ORIGINAL_NAME, weapon.getName());
+    }
+
+    @Test
+    public void getRequiredLevel_ReturnsOriginalRequiredLevel() {
+        assertEquals(ORIGINAL_LEVEL, weapon.getRequiredLevel());
+    }
+
+    @Test
+    public void getSlot_ReturnsCorrectSlot() {
+        assertEquals(Slot.WEAPON, weapon.getSlot());
+    }
 }
