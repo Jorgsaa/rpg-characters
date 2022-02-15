b03c98049e5777523252b7c754554ff7142ddbb2
Jorgsaa <jorgen.saanum@gmail.com>
2022-02-15 16:10:52 +0100

    REFACTOR: renamed `ItemEquipExceptionType` -> `ItemExceptionType`

--- src/main/java/com/jorgsaa/item/weapon/InvalidWeaponException.java
+++ src/main/java/com/jorgsaa/item/weapon/InvalidWeaponException.java
@@ -1,11 +1,11 @@
 package com.jorgsaa.item.weapon;
 
 import com.jorgsaa.character.Character;
-import com.jorgsaa.item.ItemEquipExceptionType;
+import com.jorgsaa.item.ItemExceptionType;
 
 public class InvalidWeaponException extends RuntimeException {
 
-    public InvalidWeaponException(ItemEquipExceptionType type, Character character, Weapon weapon) {
+    public InvalidWeaponException(ItemExceptionType type, Character character, Weapon weapon) {
         super(String.format(type.getMessage(), character, weapon));
     }
 
