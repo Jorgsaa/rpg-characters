f70dd94647f315b41a8bf662d280eb77b38aa966
Jorgsaa <jorgen.saanum@gmail.com>
2022-02-15 12:30:32 +0100

    FEAT: added `type` field to class `Armor`

--- src/main/java/com/jorgsaa/item/armor/Armor.java
+++ src/main/java/com/jorgsaa/item/armor/Armor.java
@@ -6,14 +6,20 @@ import com.jorgsaa.item.Slot;
 
 public class Armor extends Item {
 
+    private final ArmorType type;
     private final PrimaryAttribute attributes;
 
-    protected Armor(String name, Integer requiredLevel, Slot slot, PrimaryAttribute attributes) {
+    protected Armor(String name, Integer requiredLevel, Slot slot, ArmorType type, PrimaryAttribute attributes) {
         super(name, requiredLevel, slot);
+        this.type = type;
         this.attributes = attributes;
     }
 
     public PrimaryAttribute getAttributes() {
         return attributes;
+    }
+
+    public ArmorType getType() {
+        return type;
     }
 }
